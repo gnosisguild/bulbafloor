681d3168e34baac4a39cf810e78d5d2b1dcf950c
Auryn Macmillan <hi@auryn.xyz>
2024-01-04 22:55:12 -0500

    add: started writing subgraph tests, matchstick is throwing an error

--- packages/subgraph/tests/bulbafloor-utils.ts
+++ packages/subgraph/tests/bulbafloor-utils.ts
@@ -1,268 +1,141 @@
-import { newMockEvent } from "matchstick-as"
-import { ethereum, BigInt, Address } from "@graphprotocol/graph-ts"
+import { newMockEvent } from "matchstick-as";
+import { ethereum, BigInt, Address } from "@graphprotocol/graph-ts";
 import {
   AuctionCancelled,
   AuctionCreated,
   AuctionSuccessful,
-  FeeBasisPointsSet,
-  FeeRecipientSet,
-  Initialized,
-  Initialized1,
-  OwnershipTransferred
-} from "../generated/Bulbafloor/Bulbafloor"
+} from "../generated/Bulbafloor/Bulbafloor";
 
 export function createAuctionCancelledEvent(
   auctionId: BigInt,
   seller: Address,
   tokenContract: Address,
-  tokenId: BigInt
+  tokenId: BigInt,
 ): AuctionCancelled {
-  let auctionCancelledEvent = changetype<AuctionCancelled>(newMockEvent())
+  const auctionCancelledEvent = changetype<AuctionCancelled>(newMockEvent());
 
-  auctionCancelledEvent.parameters = new Array()
+  auctionCancelledEvent.parameters = [];
 
   auctionCancelledEvent.parameters.push(
     new ethereum.EventParam(
       "auctionId",
-      ethereum.Value.fromUnsignedBigInt(auctionId)
-    )
-  )
+      ethereum.Value.fromUnsignedBigInt(auctionId),
+    ),
+  );
   auctionCancelledEvent.parameters.push(
-    new ethereum.EventParam("seller", ethereum.Value.fromAddress(seller))
-  )
+    new ethereum.EventParam("seller", ethereum.Value.fromAddress(seller)),
+  );
   auctionCancelledEvent.parameters.push(
     new ethereum.EventParam(
       "tokenContract",
-      ethereum.Value.fromAddress(tokenContract)
-    )
-  )
+      ethereum.Value.fromAddress(tokenContract),
+    ),
+  );
   auctionCancelledEvent.parameters.push(
     new ethereum.EventParam(
       "tokenId",
-      ethereum.Value.fromUnsignedBigInt(tokenId)
-    )
-  )
+      ethereum.Value.fromUnsignedBigInt(tokenId),
+    ),
+  );
 
-  return auctionCancelledEvent
+  return auctionCancelledEvent;
 }
 
 export function createAuctionCreatedEvent(
   auctionId: BigInt,
   tokenContract: Address,
   tokenId: BigInt,
-  tokenType: i32,
+  tokenType: BigInt,
+  amount: BigInt,
   saleToken: Address,
   seller: Address,
   startPrice: BigInt,
   reservePrice: BigInt,
-  duration: BigInt
+  feeRecipient: Address,
+  feeBasisPoints: BigInt,
+  royaltyRecipient: Address,
+  royalyBasisPoints: BigInt,
+  duration: BigInt,
+  startTime: BigInt,
 ): AuctionCreated {
-  let auctionCreatedEvent = changetype<AuctionCreated>(newMockEvent())
-
-  auctionCreatedEvent.parameters = new Array()
-
-  auctionCreatedEvent.parameters.push(
+  const auctionCreatedEvent = changetype<AuctionCreated>(newMockEvent());
+  auctionCreatedEvent.parameters = [
     new ethereum.EventParam(
       "auctionId",
-      ethereum.Value.fromUnsignedBigInt(auctionId)
-    )
-  )
-  auctionCreatedEvent.parameters.push(
+      ethereum.Value.fromUnsignedBigInt(auctionId),
+    ),
+    new ethereum.EventParam(
+      "auction",
+      ethereum.Value.fromTuple([
+        ethereum.Value.fromTuple([
+          ethereum.Value.fromAddress(tokenContract),
+          ethereum.Value.fromUnsignedBigInt(tokenId),
+          ethereum.Value.fromUnsignedBigInt(tokenType),
+        ]),
+        ethereum.Value.fromUnsignedBigInt(amount),
+        ethereum.Value.fromAddress(saleToken),
+        ethereum.Value.fromAddress(seller),
+        ethereum.Value.fromUnsignedBigInt(startPrice),
+        ethereum.Value.fromUnsignedBigInt(reservePrice),
+        ethereum.Value.fromAddress(feeRecipient),
+        ethereum.Value.fromUnsignedBigInt(feeBasisPoints),
+        ethereum.Value.fromAddress(royaltyRecipient),
+        ethereum.Value.fromUnsignedBigInt(royalyBasisPoints),
+        ethereum.Value.fromUnsignedBigInt(duration),
+        ethereum.Value.fromUnsignedBigInt(startTime),
+      ]),
+    ),
+    new ethereum.EventParam("seller", ethereum.Value.fromAddress(seller)),
     new ethereum.EventParam(
       "tokenContract",
-      ethereum.Value.fromAddress(tokenContract)
-    )
-  )
-  auctionCreatedEvent.parameters.push(
-    new ethereum.EventParam(
-      "tokenId",
-      ethereum.Value.fromUnsignedBigInt(tokenId)
-    )
-  )
-  auctionCreatedEvent.parameters.push(
-    new ethereum.EventParam(
-      "tokenType",
-      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(tokenType))
-    )
-  )
-  auctionCreatedEvent.parameters.push(
-    new ethereum.EventParam("saleToken", ethereum.Value.fromAddress(saleToken))
-  )
-  auctionCreatedEvent.parameters.push(
-    new ethereum.EventParam("seller", ethereum.Value.fromAddress(seller))
-  )
-  auctionCreatedEvent.parameters.push(
-    new ethereum.EventParam(
-      "startPrice",
-      ethereum.Value.fromUnsignedBigInt(startPrice)
-    )
-  )
-  auctionCreatedEvent.parameters.push(
-    new ethereum.EventParam(
-      "reservePrice",
-      ethereum.Value.fromUnsignedBigInt(reservePrice)
-    )
-  )
-  auctionCreatedEvent.parameters.push(
-    new ethereum.EventParam(
-      "duration",
-      ethereum.Value.fromUnsignedBigInt(duration)
-    )
-  )
+      ethereum.Value.fromAddress(tokenContract),
+    ),
+  ];
 
-  return auctionCreatedEvent
+  // log each of the paramerts
+  auctionCreatedEvent.parameters.forEach((x) =>
+    console.log(x.name.concat(": ").concat(x.value.toString())),
+  );
+
+  return auctionCreatedEvent;
 }
 
 export function createAuctionSuccessfulEvent(
   auctionId: BigInt,
   seller: Address,
   buyer: Address,
-  tokenContract: Address,
-  tokenId: BigInt,
-  amount: BigInt,
-  saleToken: Address,
-  totalPrice: BigInt
+  price: BigInt,
+  fee: BigInt,
+  royalty: BigInt,
 ): AuctionSuccessful {
-  let auctionSuccessfulEvent = changetype<AuctionSuccessful>(newMockEvent())
+  const auctionSuccessfulEvent = changetype<AuctionSuccessful>(newMockEvent());
 
-  auctionSuccessfulEvent.parameters = new Array()
+  auctionSuccessfulEvent.parameters = [];
 
   auctionSuccessfulEvent.parameters.push(
     new ethereum.EventParam(
       "auctionId",
-      ethereum.Value.fromUnsignedBigInt(auctionId)
-    )
-  )
+      ethereum.Value.fromUnsignedBigInt(auctionId),
+    ),
+  );
   auctionSuccessfulEvent.parameters.push(
-    new ethereum.EventParam("seller", ethereum.Value.fromAddress(seller))
-  )
+    new ethereum.EventParam("seller", ethereum.Value.fromAddress(seller)),
+  );
   auctionSuccessfulEvent.parameters.push(
-    new ethereum.EventParam("buyer", ethereum.Value.fromAddress(buyer))
-  )
+    new ethereum.EventParam("buyer", ethereum.Value.fromAddress(buyer)),
+  );
   auctionSuccessfulEvent.parameters.push(
-    new ethereum.EventParam(
-      "tokenContract",
-      ethereum.Value.fromAddress(tokenContract)
-    )
-  )
+    new ethereum.EventParam("price", ethereum.Value.fromUnsignedBigInt(price)),
+  );
   auctionSuccessfulEvent.parameters.push(
-    new ethereum.EventParam(
-      "tokenId",
-      ethereum.Value.fromUnsignedBigInt(tokenId)
-    )
-  )
-  auctionSuccessfulEvent.parameters.push(
-    new ethereum.EventParam("amount", ethereum.Value.fromUnsignedBigInt(amount))
-  )
-  auctionSuccessfulEvent.parameters.push(
-    new ethereum.EventParam("saleToken", ethereum.Value.fromAddress(saleToken))
-  )
+    new ethereum.EventParam("fee", ethereum.Value.fromUnsignedBigInt(fee)),
+  );
   auctionSuccessfulEvent.parameters.push(
     new ethereum.EventParam(
-      "totalPrice",
-      ethereum.Value.fromUnsignedBigInt(totalPrice)
-    )
-  )
+      "royalty",
+      ethereum.Value.fromUnsignedBigInt(royalty),
+    ),
+  );
 
-  return auctionSuccessfulEvent
-}
-
-export function createFeeBasisPointsSetEvent(
-  feeBasisPoints: BigInt
-): FeeBasisPointsSet {
-  let feeBasisPointsSetEvent = changetype<FeeBasisPointsSet>(newMockEvent())
-
-  feeBasisPointsSetEvent.parameters = new Array()
-
-  feeBasisPointsSetEvent.parameters.push(
-    new ethereum.EventParam(
-      "feeBasisPoints",
-      ethereum.Value.fromUnsignedBigInt(feeBasisPoints)
-    )
-  )
-
-  return feeBasisPointsSetEvent
-}
-
-export function createFeeRecipientSetEvent(
-  feeRecipient: Address
-): FeeRecipientSet {
-  let feeRecipientSetEvent = changetype<FeeRecipientSet>(newMockEvent())
-
-  feeRecipientSetEvent.parameters = new Array()
-
-  feeRecipientSetEvent.parameters.push(
-    new ethereum.EventParam(
-      "feeRecipient",
-      ethereum.Value.fromAddress(feeRecipient)
-    )
-  )
-
-  return feeRecipientSetEvent
-}
-
-export function createInitializedEvent(version: BigInt): Initialized {
-  let initializedEvent = changetype<Initialized>(newMockEvent())
-
-  initializedEvent.parameters = new Array()
-
-  initializedEvent.parameters.push(
-    new ethereum.EventParam(
-      "version",
-      ethereum.Value.fromUnsignedBigInt(version)
-    )
-  )
-
-  return initializedEvent
-}
-
-export function createInitialized1Event(
-  owner: Address,
-  feeBasisPoints: i32,
-  feeRecipient: Address
-): Initialized1 {
-  let initialized1Event = changetype<Initialized1>(newMockEvent())
-
-  initialized1Event.parameters = new Array()
-
-  initialized1Event.parameters.push(
-    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
-  )
-  initialized1Event.parameters.push(
-    new ethereum.EventParam(
-      "feeBasisPoints",
-      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(feeBasisPoints))
-    )
-  )
-  initialized1Event.parameters.push(
-    new ethereum.EventParam(
-      "feeRecipient",
-      ethereum.Value.fromAddress(feeRecipient)
-    )
-  )
-
-  return initialized1Event
-}
-
-export function createOwnershipTransferredEvent(
-  previousOwner: Address,
-  newOwner: Address
-): OwnershipTransferred {
-  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
-    newMockEvent()
-  )
-
-  ownershipTransferredEvent.parameters = new Array()
-
-  ownershipTransferredEvent.parameters.push(
-    new ethereum.EventParam(
-      "previousOwner",
-      ethereum.Value.fromAddress(previousOwner)
-    )
-  )
-  ownershipTransferredEvent.parameters.push(
-    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
-  )
-
-  return ownershipTransferredEvent
+  return auctionSuccessfulEvent;
 }
--- packages/subgraph/tests/bulbafloor.test.ts
+++ packages/subgraph/tests/bulbafloor.test.ts
@@ -4,73 +4,73 @@ import {
   test,
   clearStore,
   beforeAll,
-  afterAll
-} from "matchstick-as/assembly/index"
-import { BigInt, Address } from "@graphprotocol/graph-ts"
-import { AuctionCancelled } from "../generated/schema"
-import { AuctionCancelled as AuctionCancelledEvent } from "../generated/Bulbafloor/Bulbafloor"
-import { handleAuctionCancelled } from "../src/bulbafloor"
-import { createAuctionCancelledEvent } from "./bulbafloor-utils"
+  afterAll,
+} from "matchstick-as/assembly/index";
+import { BigInt, Address } from "@graphprotocol/graph-ts";
+import { AuctionCancelled } from "../generated/schema";
+import { AuctionCancelled as AuctionCancelledEvent } from "../generated/Bulbafloor/Bulbafloor";
+import { handleAuctionCancelled } from "../src/bulbafloor";
+import { createAuctionCancelledEvent } from "./bulbafloor-utils";
 
 // Tests structure (matchstick-as >=0.5.0)
 // https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
 
 describe("Describe entity assertions", () => {
   beforeAll(() => {
-    let auctionId = BigInt.fromI32(234)
+    let auctionId = BigInt.fromI32(234);
     let seller = Address.fromString(
-      "0x0000000000000000000000000000000000000001"
-    )
+      "0x0000000000000000000000000000000000000001",
+    );
     let tokenContract = Address.fromString(
-      "0x0000000000000000000000000000000000000001"
-    )
-    let tokenId = BigInt.fromI32(234)
+      "0x0000000000000000000000000000000000000001",
+    );
+    let tokenId = BigInt.fromI32(234);
     let newAuctionCancelledEvent = createAuctionCancelledEvent(
       auctionId,
       seller,
       tokenContract,
-      tokenId
-    )
-    handleAuctionCancelled(newAuctionCancelledEvent)
-  })
+      tokenId,
+    );
+    handleAuctionCancelled(newAuctionCancelledEvent);
+  });
 
   afterAll(() => {
-    clearStore()
-  })
+    clearStore();
+  });
 
   // For more test scenarios, see:
   // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
 
   test("AuctionCancelled created and stored", () => {
-    assert.entityCount("AuctionCancelled", 1)
+    assert.entityCount("AuctionCancelled", 1);
 
     // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
     assert.fieldEquals(
       "AuctionCancelled",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "auctionId",
-      "234"
-    )
+      "234",
+    );
     assert.fieldEquals(
       "AuctionCancelled",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "seller",
-      "0x0000000000000000000000000000000000000001"
-    )
+      "0x0000000000000000000000000000000000000001",
+    );
     assert.fieldEquals(
       "AuctionCancelled",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "tokenContract",
-      "0x0000000000000000000000000000000000000001"
-    )
+      "0x0000000000000000000000000000000000000001",
+    );
     assert.fieldEquals(
       "AuctionCancelled",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "tokenId",
-      "234"
-    )
+      "234",
+    );
 
     // More assert options:
     // https://thegraph.com/docs/en/developer/matchstick/#asserts
-  })
-})
+  });
+});
